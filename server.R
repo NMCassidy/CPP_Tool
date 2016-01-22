cbf5fb876d1abfa61431f68017fbd72ddf46cc9c
Nick Cassidy <Nick Cassidy>
2016-01-22 13:20:12 +0000

    added data

--- server.R
+++ server.R
@@ -2,8 +2,9 @@ library(shiny)
 library(ggplot2)
 shinyServer(
   function(input,output){
-   
     
+   
+   jsa_dta<- read.csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1_1.data.csv?geography=1879048541...1879048572&date=latestMINUS5-latest&sex=7&item=1&measures=20100,20203&select=date_name,geography_name,geography_code,sex_name,item_name,measures_name,obs_value,obs_status_name")
     data<-reactive({
      File<-input$fl
     if(is.null(File)){
