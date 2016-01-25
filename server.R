d36ac2af7d1410a39d07a2882056ba8f6a0faa03
Nicholas Cassidy <nicholas.cassidy@improvementservice.org.uk>
2016-01-25 11:53:40 +0000

    Created bar chart and added link to download Scotland JSA data

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(ggplot2)
-pcchange=function(x,lag=1) c(rep(NA,lag),diff(x,lag))/x
+library(reshape2)
+pcchange=function(x,lag=1) c(rep(0,lag),diff(x,lag))/x
 shinyServer(
   function(input,output){
     
@@ -8,7 +9,8 @@ shinyServer(
    jsa_dta<- read.csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1_1.data.csv?geography=1879048541...1879048572&date=latestMINUS5-latest&sex=7&item=1&measures=20100,20203&select=date_name,geography_name,geography_code,sex_name,item_name,measures_name,obs_value,obs_status_name")
    AC_dta<-subset(jsa_dta, subset = GEOGRAPHY_NAME == "Aberdeen City") 
    AC_tot<-subset(AC_dta, subset = MEASURES_NAME == "Persons claiming JSA", select = c(1,2,6,7))
-   
+   jsa_sco<-read.csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1_1.data.csv?geography=2092957701&date=latestMINUS5-latest&sex=7&item=1&measures=20100,20203&select=date_name,geography_name,geography_code,sex_name,item_name,measures_name,obs_value,obs_status_name")
+   sco_tot<-subset(jsa_sco, subset = MEASURES_NAME == "Persons claiming JSA", select=c(1,2,6,7))
    data<-reactive({
      File<-input$fl
     if(is.null(File)){
@@ -33,11 +35,22 @@ shinyServer(
     })
     output$relchange_perc<-renderPlot({
       AC_tot$relchange<-pcchange(AC_tot$OBS_VALUE)
+      sco_tot$relchange<-pcchange(sco_tot$OBS_VALUE)
       ggplot(data = AC_tot, aes(x = factor(DATE_NAME,levels = AC_tot$DATE_NAME), y = relchange, group = 1))+
         geom_smooth(size = 2)+theme_bw()+geom_hline(yintercept = 0)+
+        geom_smooth(data = sco_tot, aes(x = factor(DATE_NAME,levels = sco_tot$DATE_NAME), y = relchange, group = 1, fill = GEOGRAPHY_NAME))+
         xlab("Date")+ylab("Year-on-year Change")
+      
+  mrg_tot<-rbind(AC_tot[1:5], sco_tot)    
+  mrg_tot<-mrg_tot[c(1,2,5)]
+  mrg_tot<-melt(mrg_tot)
+    ggplot(data = mrg_tot, aes(x = factor(DATE_NAME,levels = mrg_tot$DATE_NAME), y = value, fill = GEOGRAPHY_NAME))+
+     geom_bar(position = "dodge", stat = "identity")+
+    theme_bw()+geom_hline(yintercept = 0)+
+    xlab("Date")+ylab("Year-on-year Change")
       })
-    output$totchange_perc<-renderPlot({
+   
+     output$totchange_perc<-renderPlot({
      AC_tot$abschange<-c(1:nrow(AC_tot))
       for(i in 1:nrow(AC_tot)){
        AC_tot[i,6]<-AC_tot[i,4]/AC_tot[1,4]*100
