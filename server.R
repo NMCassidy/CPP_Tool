20568e0bd1657d3b19b6778140a2ea58ada47501
Nicholas Cassidy <nicholas.cassidy@improvementservice.org.uk>
2016-01-25 15:25:17 +0000

    updated ui and output names

--- server.R
+++ server.R
@@ -5,23 +5,23 @@ pcchange=function(x,lag=1) c(rep(0,lag),diff(x,lag))/x
 shinyServer(
   function(input,output){
     
-   
-   jsa_dta<- read.csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1_1.data.csv?geography=1879048541...1879048572&date=latestMINUS5-latest&sex=7&item=1&measures=20100,20203&select=date_name,geography_name,geography_code,sex_name,item_name,measures_name,obs_value,obs_status_name")
-   AC_dta<-subset(jsa_dta, subset = GEOGRAPHY_NAME == "Aberdeen City") 
-   AC_tot<-subset(AC_dta, subset = MEASURES_NAME == "Persons claiming JSA", select = c(1,2,6,7))
-   jsa_sco<-read.csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1_1.data.csv?geography=2092957701&date=latestMINUS5-latest&sex=7&item=1&measures=20100,20203&select=date_name,geography_name,geography_code,sex_name,item_name,measures_name,obs_value,obs_status_name")
-   sco_tot<-subset(jsa_sco, subset = MEASURES_NAME == "Persons claiming JSA", select=c(1,2,6,7))
-   data<-reactive({
-     File<-input$fl
-    if(is.null(File)){
-    return(NULL)}
     
-    read.csv(File$datapath, header = TRUE)
-  })
+    jsa_dta<- read.csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1_1.data.csv?geography=1879048541...1879048572&date=latestMINUS5-latest&sex=7&item=1&measures=20100,20203&select=date_name,geography_name,geography_code,sex_name,item_name,measures_name,obs_value,obs_status_name")
+    AC_dta<-subset(jsa_dta, subset = GEOGRAPHY_NAME == "Aberdeen City") 
+    AC_tot<-subset(AC_dta, subset = MEASURES_NAME == "Persons claiming JSA", select = c(1,2,6,7))
+    jsa_sco<-read.csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_1_1.data.csv?geography=2092957701&date=latestMINUS5-latest&sex=7&item=1&measures=20100,20203&select=date_name,geography_name,geography_code,sex_name,item_name,measures_name,obs_value,obs_status_name")
+    sco_tot<-subset(jsa_sco, subset = MEASURES_NAME == "Persons claiming JSA", select=c(1,2,6,7))
+    data<-reactive({
+      File<-input$fl
+      if(is.null(File)){
+        return(NULL)}
+      
+      read.csv(File$datapath, header = TRUE)
+    })
     
     graph<-reactive({
-    ggplot(data = data(), aes(x = Local.Authority, y = Local.Authority.Geographical.Area...2005))+
-             geom_bar(stat = "identity")
+      ggplot(data = data(), aes(x = Local.Authority, y = Local.Authority.Geographical.Area...2005))+
+        geom_bar(stat = "identity")
     })
     
     output$graph<-renderPlot({
@@ -33,31 +33,33 @@ shinyServer(
         geom_bar(stat = "identity")+
         theme_bw()+ xlab("Date")+ylab("Number of JSA Claimants")
     })
-    output$relchange_perc<-renderPlot({
+    output$relchange_line<-renderPlot({
       AC_tot$relchange<-pcchange(AC_tot$OBS_VALUE)
       sco_tot$relchange<-pcchange(sco_tot$OBS_VALUE)
       ggplot(data = AC_tot, aes(x = factor(DATE_NAME,levels = AC_tot$DATE_NAME), y = relchange, group = 1))+
-        geom_smooth(size = 2)+theme_bw()+geom_hline(yintercept = 0)+
+        geom_smooth(size = 1)+theme_bw()+geom_hline(yintercept = 0)+
         geom_smooth(data = sco_tot, aes(x = factor(DATE_NAME,levels = sco_tot$DATE_NAME), y = relchange, group = 1, fill = GEOGRAPHY_NAME))+
         xlab("Date")+ylab("Year-on-year Change")
-      
-  mrg_tot<-rbind(AC_tot[1:5], sco_tot)    
-  mrg_tot<-mrg_tot[c(1,2,5)]
-  mrg_tot<-melt(mrg_tot)
-    ggplot(data = mrg_tot, aes(x = factor(DATE_NAME,levels = mrg_tot$DATE_NAME), y = value, fill = GEOGRAPHY_NAME))+
-     geom_bar(position = "dodge", stat = "identity")+
-    theme_bw()+geom_hline(yintercept = 0)+
-    xlab("Date")+ylab("Year-on-year Change")
-      })
-   
-     output$totchange_perc<-renderPlot({
-     AC_tot$abschange<-c(1:nrow(AC_tot))
+    })
+    
+    output$relchange_bar<-renderPlot({
+      mrg_tot<-rbind(AC_tot[1:5], sco_tot)    
+      mrg_tot<-mrg_tot[c(1,2,5)]
+      mrg_tot<-melt(mrg_tot)
+      ggplot(data = mrg_tot, aes(x = factor(DATE_NAME,levels = mrg_tot$DATE_NAME), y = value, fill = GEOGRAPHY_NAME))+
+        geom_bar(position = "dodge", stat = "identity")+
+        theme_bw()+geom_hline(yintercept = 0)+
+        xlab("Date")+ylab("Year-on-year Change")
+    })
+    
+    output$totchange_perc<-renderPlot({
+      AC_tot$abschange<-c(1:nrow(AC_tot))
       for(i in 1:nrow(AC_tot)){
-       AC_tot[i,6]<-AC_tot[i,4]/AC_tot[1,4]*100
+        AC_tot[i,6]<-AC_tot[i,4]/AC_tot[1,4]*100
       }
-     ggplot(data = AC_tot, aes(x = factor(DATE_NAME,levels = AC_tot$DATE_NAME), y = abschange, group = 1))+
-       geom_smooth(size = 1)+theme_bw()+geom_hline(yintercept = 0)+
-       xlab("Date")+ylab("Year-on-year Change")+ylim(c(90,110))
+      ggplot(data = AC_tot, aes(x = factor(DATE_NAME,levels = AC_tot$DATE_NAME), y = abschange, group = 1))+
+        geom_smooth(size = 1)+theme_bw()+geom_hline(yintercept = 0)+
+        xlab("Date")+ylab("Year-on-year Change")+ylim(c(90,110))
     })
     
     output$downloadPlot <- downloadHandler(
